621509fef02d94a1c5ca116752f335842a1f4eef
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-01-13 15:18:52 +1100

    Display amount and description entered
    If the user has not entered anything, default values will appear

--- ExpenseTracker/ViewController.swift
+++ ExpenseTracker/ViewController.swift
@@ -6,16 +6,22 @@
 //  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
 //
 
+/*  Questions
+    How do I customise the keyboard? The keyboard I want is not in the attributes list.
+    I'd like number pad with a decimal point.
+*/
+
 import UIKit
 
 class ViewController: UIViewController {
+
+    // MARK: Variables
     @IBOutlet private weak var amountEntered: UITextField!
     @IBOutlet private weak var descriptionLabel: UITextField!
     @IBOutlet private weak var testLabel: UILabel!
 
     
-    
-    
+    // MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,6 +32,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showExpenseButton(_ sender: UIButton) {
+        var displayAmount = "0.00"
+        var displayDescription = "nothing"
+        
+        if let amount = amountEntered.text, !amount.isEmpty {
+            displayAmount = amount
+        }
+    
+        if let label = descriptionLabel.text, !label.isEmpty {
+            displayDescription = label
+        }
+ 
+        testLabel.text = "You spent $\(displayAmount) on \(displayDescription)"        
+    }
 
 }
 
