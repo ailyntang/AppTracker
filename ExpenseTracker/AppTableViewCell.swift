153460b6bd855a5feb4f0254c9596d9686e69570
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-01-23 11:03:02 +1100

    Add table view with list of fruit names

---
+++ ExpenseTracker/AppTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AppTableViewCell.swift
+//  ExpenseTracker
+//
+//  Created by Ai-Lyn Tang on 20/1/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import UIKit
+
+class AppTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
