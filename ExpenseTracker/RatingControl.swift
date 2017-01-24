ab89819342aff521c3c86253b67b09a79fe5329a
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-01-24 16:39:22 +1100

    Trying to debug rating control in Review Table View Cell
    The rating control is always showing 2 out of 5 star ratings. The rating control on the App Table View Cell is working correctly. I have deleted what I thought was unnecessary code, but maybe it was more necessary than I thought.

--- ExpenseTracker/RatingControl.swift
+++ ExpenseTracker/RatingControl.swift
@@ -12,7 +12,12 @@ class RatingControl: UIStackView {
     
     // MARK: Properties
     private var ratingButtons = [UIButton]()
+
+//    var rating = 0
     
+    // If I comment out this section, then the stars are all empty stars
+    // When I use the didSet updateButtonSelectionStates, the stars on the "list of apps" are filled in correctly
+    // However the stars on the "reviews" scene are all 2 filled, 3 empty
     var rating = 0 {
         didSet {
             updateButtonSelectionStates()
@@ -30,26 +35,6 @@ class RatingControl: UIStackView {
         super.init(coder: coder)
         setupButtons()
     }
-    
-    
-    // MARK: Button Action
-//    func ratingButtonTapped(button: UIButton) {
-//        guard let index = ratingButtons.index(of: button) else {
-//            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
-//        }
-//        
-//        // Calculate the rating of the selected button
-//        let selectedRating = index + 1
-//        
-//        if selectedRating == rating {
-//            // If the selected star represents the current rating, reset ther ating to 0
-//            rating = 0
-//        } else {
-//            // Otherwise set the rating to the selected star
-//            rating = selectedRating
-//        }
-//        
-//    }
     
     
     // MARK: Private Methods
@@ -72,20 +57,9 @@ class RatingControl: UIStackView {
             // Create the button
             let button = UIButton()
             
-            
             // Set button images
             button.setImage(emptyStar, for: .normal)
             button.setImage(filledStar, for: .selected)
-            
-            // Add constraints
-            // These contraints determine the size of the stars in the ReviewTableViewCell
-//            button.translatesAutoresizingMaskIntoConstraints = false
-//            button.heightAnchor.constraint(equalToConstant: 14.0).isActive = true
-//            button.widthAnchor.constraint(equalToConstant: 14.0).isActive = true
-            
-            // Setup the button action
-            // This is for setting the rating. But for my app, the rating will always be read only.
-//            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
             
             // Add the button to the stack
             addArrangedSubview(button)
@@ -94,7 +68,9 @@ class RatingControl: UIStackView {
             ratingButtons.append(button)
         }
         
-        updateButtonSelectionStates()
+
+        // This doesn't seem to do anything. it doesn't hinder or help with the right number of stars on "list of apps" or "reviews"
+//        updateButtonSelectionStates()
     }
     
     private func updateButtonSelectionStates() {
@@ -104,14 +80,4 @@ class RatingControl: UIStackView {
         }
     }
     
-    
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
