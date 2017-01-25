34ccff298b30ce895f6463fd86fbe8c7b3988312
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-01-25 14:14:10 +1100

    RatingControl changed buttons to imageviews
    Do not have the rating working yet. It defaults to 5 stars of the same image, irrespective of the rating. This is because I commented out the button code in "updateButtonSelectionStates()

--- ExpenseTracker/RatingControl.swift
+++ ExpenseTracker/RatingControl.swift
@@ -11,7 +11,8 @@ import UIKit
 class RatingControl: UIStackView {
     
     // MARK: Properties
-    private var ratingButtons = [UIButton]()
+//    private var ratingButtons = [UIButton]()
+    private var ratingImages = [UIImageView]()
     
     // Critical, don't delete. If I comment out this section, then the stars are all empty stars
     var rating = 0.0 {
@@ -21,7 +22,80 @@ class RatingControl: UIStackView {
     }
     
     
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    
+    // MARK: Private Methods
+    private func setupButtons() {
+        
+//        // Clear any existing buttons
+//        for button in ratingButtons {
+//            removeArrangedSubview(button)
+//            button.removeFromSuperview()
+//        }
+        ratingImages.removeAll()
+        
+        // Load button images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let halfStar = UIImage(named: "halfStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        
+        
+        
+        for _ in 0..<5 {
+            // Create the button
+            let image = UIImageView(image: halfStar)
+            
+            // Determine half star vs filled star
+//            if fmod(rating, 1) == 0 {
+//                button = filledStar!
+//                
+//            }
+            // Add the button to the stack
+            addArrangedSubview(image)
+            
+            // Add the new button to the rating button array
+            ratingImages.append(image)
+        }
 
+        updateButtonSelectionStates()
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index, image) in ratingImages.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+//            button.isSelected = Double(index) < rating
+        }
+    }
+    
+}
+
+
+/*
+// This code all works
+class RatingControl: UIStackView {
+    
+    // MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    // Critical, don't delete. If I comment out this section, then the stars are all empty stars
+    var rating = 0.0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
+    
     
     // MARK: Initialization
     override init(frame: CGRect) {
@@ -49,20 +123,18 @@ class RatingControl: UIStackView {
         let bundle = Bundle(for: type(of: self))
         let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
         let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
-//        let halfStar = UIImage(named: "halfStar", in: bundle, compatibleWith: self.traitCollection)
+        //        let halfStar = UIImage(named: "halfStar", in: bundle, compatibleWith: self.traitCollection)
         
         
         
         
         for _ in 0..<5 {
-            
             // Create the button
             let button = UIButton()
             
-            
             // Set button images
             button.setImage(emptyStar, for: .normal)
-//            button.setImage(filledStar, for: .selected)
+            //            button.setImage(filledStar, for: .selected)
             
             // Determine half star vs filled star
             // % doesn't work for double
@@ -75,30 +147,8 @@ class RatingControl: UIStackView {
             
             // Add the new button to the rating button array
             ratingButtons.append(button)
-            
         }
         
-        
-        
-//        // This is the original for loop
-//        for _ in 0..<5 {
-//            // Create the button
-//            let button = UIButton()
-//            
-//            // Set button images
-//            button.setImage(emptyStar, for: .normal)
-//            button.setImage(filledStar, for: .selected)
-//            button.setImage(halfStar, for: .selected)
-//            
-//            // Add the button to the stack
-//            addArrangedSubview(button)
-//            
-//            // Add the new button to the rating button array
-//            ratingButtons.append(button)
-//        }
-        
-        // This doesn't seem to do anything. it doesn't hinder or help with the right number of stars on "list of apps" or "reviews"
-        // However it seems most logical to put it here rather than in the var rating declaration, so I've left it in,
         updateButtonSelectionStates()
     }
     
@@ -110,3 +160,4 @@ class RatingControl: UIStackView {
     }
     
 }
+*/
