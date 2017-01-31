1948879f72fb8373d68b62f1acde7c1181030ebb
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-01-31 13:32:01 +1100

    Move loadSampleReview() function from View Controller into Review class

--- ExpenseTracker/Review.swift
+++ ExpenseTracker/Review.swift
@@ -19,6 +19,7 @@ class Review {
     var review: String
     
     
+    
     // MARK: Initialization
     
     init?(appName: String, reviewDate: String, reviewRating: Double, title: String, review: String) {
@@ -38,5 +39,41 @@ class Review {
         self.title = title
         self.review = review
     }
+    
+    
+    class func loadSampleReviews() -> [Review] {
+        
+        var reviews: [Review] = []
+        
+        // Fetch URL
+        let url = Bundle.main.url(forResource: "sampleReviews", withExtension: "json")!
+        
+        // Load data
+        let data = try! Data(contentsOf: url)
+        
+        // Deserialize JSON
+        let topLevelDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as! NSDictionary
+        
+        let reviewList = topLevelDictionary["reviews"] as! NSArray
+        
+        
+        // Convert the data into an array of Reviews
+        for anyReview in reviewList {
+            let reviewDictionary = anyReview as! NSDictionary
+            
+            let appName = reviewDictionary["appName"] as? String
+            let reviewDate = reviewDictionary["reviewDate"] as? String
+            let reviewRating = reviewDictionary["reviewRating"] as? Double
+            let title = reviewDictionary["title"] as? String
+            let review = reviewDictionary["review"] as? String
+            
+            let myReview = Review(appName: appName!, reviewDate: reviewDate!, reviewRating: reviewRating!, title: title!, review: review!)
+            
+            reviews.append(myReview!)
+        }
+        
+        return reviews
+    }
+    
     
 }
--- ExpenseTracker/ReviewTableViewController.swift
+++ ExpenseTracker/ReviewTableViewController.swift
@@ -15,8 +15,7 @@ class ReviewTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadSampleReviews()
-
+        reviews = Review.loadSampleReviews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,12 +23,14 @@ class ReviewTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return reviews.count
     }
@@ -45,42 +46,5 @@ class ReviewTableViewController: UITableViewController {
         return cell
         
     }
-    
-    
-    // MARK: Private Methods
-    
-    private func loadSampleReviews() {
-
-        // Fetch URL
-        let url = Bundle.main.url(forResource: "sampleReviews", withExtension: "json")!
-        
-        // Load data
-        let data = try! Data(contentsOf: url)
-        
-        // Deserialize JSON
-        let topLevelDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as! NSDictionary
-
-        let reviewList = topLevelDictionary["reviews"] as! NSArray
-        
-        
-        // Convert the data into an array of Reviews
-        for anyReview in reviewList {
-            let reviewDictionary = anyReview as! NSDictionary
-            
-            let appName = reviewDictionary["appName"] as? String
-            let reviewDate = reviewDictionary["reviewDate"] as? String
-            let reviewRating = reviewDictionary["reviewRating"] as? Double
-            let title = reviewDictionary["title"] as? String
-            let review = reviewDictionary["review"] as? String
-            
-            let myReview = Review(appName: appName!, reviewDate: reviewDate!, reviewRating: reviewRating!, title: title!, review: review!)
-            
-            reviews.append(myReview!)
-        }
-
-        
-    }
-    
-    
 
 }
