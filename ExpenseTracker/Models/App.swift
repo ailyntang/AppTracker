cff39fbff8e9d8e9ceb4fc6a1781688f0ca5c66d
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-02-03 10:55:32 +1100

    Modify loadSampleApps to use SwiftyJSON framework

--- ExpenseTracker/Models/App.swift
+++ ExpenseTracker/Models/App.swift
@@ -13,10 +13,10 @@ class App {
     // MARK: Properties
     
     var appName: String
-    var appIconUrl: String                       // This should be a URL not an image
+    var appIconUrl: String
     var currentVersionRating: Double?
     var latestReleaseDate: Date                 // the initialisation takes a date string and returns a date
-
+    
     
     // MARK: Initialization
     
@@ -45,40 +45,73 @@ class App {
     
     
     // MARK: Functions
-    
-    class func appFromJson(dict: NSDictionary) -> App {
-        let appName = dict["name"] as! String
-        let appIconUrl = dict["photo"] as! String
-        let currentVersionRating = dict["rating"] as! Double?
-        let latestReleaseDate = dict["date"] as! String
-        
-        let app = App(appName: appName, appIconUrl: appIconUrl, currentVersionRating: currentVersionRating, latestReleaseDate: latestReleaseDate)
-        return app!
-        
-    }
-    
-    
-    class func loadSampleApps() -> [App] {
-        
+
+    class func appsFromJson(json: JSON) -> [App]{
         var apps: [App] = []
         
-        let url = Bundle.main.url(forResource: "sampleApps", withExtension: "json")!
-        let data = try! Data(contentsOf: url)
-        
-        let topLevelDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
-        let appList = topLevelDictionary["apps"] as! NSArray
-        
-        for anyApp in appList {
-            let appDictionary = anyApp as! NSDictionary
-            let myApp = appFromJson(dict: appDictionary)
-            apps.append(myApp)
+        if let appList = json["apps"].array {
+            for app in appList {
+                let appName = app["name"].string
+                let appIconUrl = app["photo"].string
+                let currentVersionRating = app["rating"].double
+                let latestReleaseDate = app["date"].string
+                
+                let newApp = App(appName: appName!, appIconUrl: appIconUrl!, currentVersionRating: currentVersionRating, latestReleaseDate: latestReleaseDate!)
+                apps.append(newApp!)
+            }
         }
-    
+        
         return apps
     }
+
+    
+    class func loadSampleApps() -> [App] {
+
+        let url = Bundle.main.url(forResource: "sampleApps", withExtension: "json")
+        let data = try! Data(contentsOf: url!)
+        let json = JSON(data: data)
+        
+        return appsFromJson(json: json)
+        
+    }
+}
+
+        
+   
+        // MARK: Functions without SwiftyJSON
+        
+//    class func appFromJson(dict: NSDictionary) -> App {
+//        let appName = dict["name"] as! String
+//        let appIconUrl = dict["photo"] as! String
+//        let currentVersionRating = dict["rating"] as! Double?
+//        let latestReleaseDate = dict["date"] as! String
+//
+//        let app = App(appName: appName, appIconUrl: appIconUrl, currentVersionRating: currentVersionRating, latestReleaseDate: latestReleaseDate)
+//        return app!
+//        
+//    }
+//        class func loadSampleApps() -> [App] {
+        
+//        // Original JSON parsing without SwiftyJSON, all works fine
+//        var apps: [App] = []
+//        
+//        let url = Bundle.main.url(forResource: "sampleApps", withExtension: "json")!
+//        let data = try! Data(contentsOf: url)
+//        
+//        let topLevelDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
+//        let appList = topLevelDictionary["apps"] as! NSArray
+//        
+//        for anyApp in appList {
+//            let appDictionary = anyApp as! NSDictionary
+//            let myApp = appFromJson(dict: appDictionary)
+//            apps.append(myApp)
+//        }
+//    
+//        return apps
+    
     
     
 
     
     
-}
+
