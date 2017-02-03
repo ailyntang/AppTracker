4981d94b1268d3eb5ed4fe4b1f9629dc4f8a9fd0
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-02-03 11:18:28 +1100

    Modify loadSampleReview to use SwiftyJSON

--- ExpenseTracker/Models/Review.swift
+++ ExpenseTracker/Models/Review.swift
@@ -22,7 +22,7 @@ class Review {
     
     init?(appName: String, reviewDate: String, reviewRating: Double, reviewTitle: String?, reviewInFull: String?) {
     
-        let unwrappedReviewTitle = reviewTitle ?? "No review"
+        let unwrappedReviewTitle = reviewTitle ?? ""
         let unwrappedReviewInFull = reviewInFull ?? ""
         
         
@@ -44,39 +44,76 @@ class Review {
     }
     
     
-    class func loadSampleReviews() -> [Review] {
-        
+    // MARK: Functions
+    
+    class func reviewsFromJson(json: JSON) -> [Review] {
         var reviews: [Review] = []
         
-        // Fetch URL
-        let url = Bundle.main.url(forResource: "sampleReviews", withExtension: "json")!
-        
-        // Load data
-        let data = try! Data(contentsOf: url)
-        
-        // Deserialize JSON
-        let topLevelDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as! NSDictionary
-        
-        let reviewList = topLevelDictionary["reviews"] as! NSArray
-        
-        
-        // Convert the data into an array of Reviews
-        for anyReview in reviewList {
-            let reviewDictionary = anyReview as! NSDictionary
-            
-            let appName = reviewDictionary["appName"] as? String
-            let reviewDate = reviewDictionary["reviewDate"] as? String
-            let reviewRating = reviewDictionary["reviewRating"] as? Double
-            let reviewTitle = reviewDictionary["title"] as? String?
-            let reviewInFull = reviewDictionary["review"] as? String?
-            
-            let myReview = Review(appName: appName!, reviewDate: reviewDate!, reviewRating: reviewRating!, reviewTitle: reviewTitle!, reviewInFull: reviewInFull!)
-            
-            reviews.append(myReview!)
+        if let reviewList = json["reviews"].array {
+            for review in reviewList {
+                let appName = review["appName"].string
+                let reviewDate = review["reviewDate"].string
+                let reviewRating = review["reviewRating"].double
+                let reviewTitle = review["title"].string
+                let reviewInFull = review["review"].string
+                
+                let newReview = Review(appName: appName!, reviewDate: reviewDate!, reviewRating: reviewRating!, reviewTitle: reviewTitle, reviewInFull: reviewInFull)
+                
+                reviews.append(newReview!)
+            }
         }
         
         return reviews
     }
     
     
+    class func loadSampleReviews() -> [Review] {
+        
+        let url = Bundle.main.url(forResource: "sampleReviews", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        let json = JSON(data: data)
+        
+        return reviewsFromJson(json: json)
+        
+    }
+    
+    
+    
 }
+
+
+    // MARK: Functions without SwiftyJSON
+
+//class func loadSampleReviews() -> [Review] {
+//    
+//    var reviews: [Review] = []
+//    
+//    // Fetch URL
+//    let url = Bundle.main.url(forResource: "sampleReviews", withExtension: "json")!
+//    
+//    // Load data
+//    let data = try! Data(contentsOf: url)
+//    
+//    // Deserialize JSON
+//    let topLevelDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as! NSDictionary
+//    
+//    let reviewList = topLevelDictionary["reviews"] as! NSArray
+//    
+//    
+//    // Convert the data into an array of Reviews
+//    for anyReview in reviewList {
+//        let reviewDictionary = anyReview as! NSDictionary
+//        
+//        let appName = reviewDictionary["appName"] as? String
+//        let reviewDate = reviewDictionary["reviewDate"] as? String
+//        let reviewRating = reviewDictionary["reviewRating"] as? Double
+//        let reviewTitle = reviewDictionary["title"] as? String?
+//        let reviewInFull = reviewDictionary["review"] as? String?
+//        
+//        let myReview = Review(appName: appName!, reviewDate: reviewDate!, reviewRating: reviewRating!, reviewTitle: reviewTitle!, reviewInFull: reviewInFull!)
+//        
+//        reviews.append(myReview!)
+//    }
+//    
+//    return reviews
+//}
